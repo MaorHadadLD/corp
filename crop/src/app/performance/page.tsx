d14028bc1140507bf48660de1a2060ee1021b55a
MaorHadadLD <maorhadad94@gmail.com>
2024-12-02 22:41:19 +0200

    update scale page

--- crop/src/app/performance/page.tsx
+++ crop/src/app/performance/page.tsx
@@ -1,10 +1,10 @@
-import homeImg from '../../../public/performance.jpg';
+import performanceImg from '../../../public/performance.jpg';
 import Hero from '@/components/hero';
 
 export default function PerformancePage() {
     return (
     <Hero 
-        imgData={homeImg} 
+        imgData={performanceImg} 
         imgAlt="welding" 
         title="We serve high performance applications" 
       />
